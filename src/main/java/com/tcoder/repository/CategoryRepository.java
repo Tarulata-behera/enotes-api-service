ee2ec378eac9233e8eda4fabefc0f34bbe7163ed
Tarulata Behera <tarulatabehera4@gmail.com>
2025-09-28 12:19:35 +0530

    save get category implement

---
+++ src/main/java/com/tcoder/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.tcoder.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tcoder.entity.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer>{
+
+}
